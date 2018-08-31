5caeeb4620e0439666259d59291c7c4991078004
Toon Verbeek <toon@homerun.co>
2018-08-31 11:59:00 +0200

    refactor

--- src/components/elements/card.js
+++ src/components/elements/card.js
@@ -2,26 +2,6 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class CardElement extends Component {
-  //   componentWillReceiveProps(nextProps) {
-  //     console.log("hoi");
-  //     return this.props !== nextProps;
-  //   }
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      height: props.height,
-      width: props.width,
-      x: props.x,
-      y: props.y,
-      color: props.color,
-      selected: props.selected
-    };
-  }
-  UNSAFE_componentWillReceiveProps() {
-    console.log("hoi");
-  }
   render() {
     const colors = [
       "grey",
@@ -40,14 +20,17 @@ class CardElement extends Component {
       this.props.color > 9
         ? colors[10 - this.props.color]
         : colors[this.props.color];
+
+    let x, y, height, width;
+    ({ x, y, height, width } = this.props);
+
     return (
       <div
         className={`${
           this.props.selected ? "border-2 border-black resize" : ""
         } bg-${color}-lightest bg-grey-lightest cursor-pointer`}
         style={{
-          gridArea: `${this.props.y}/${this.props.x} / ${this.props.y +
-            this.props.height}/${this.props.x + this.props.width}` //grid-row-start / grid-col-start / grid-row-end / grid-col-end
+          gridArea: `${y}/${x} / ${y + height}/${x + width}` //grid-row-start / grid-col-start / grid-row-end / grid-col-end
         }}
         onClick={this.props.onClick}
       />
