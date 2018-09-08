028dfde0067439a8f1c47b14f9980bf8d73f5289
Toon Verbeek <toon.verbeek1@gmail.com>
2018-09-08 19:56:18 +0200

    major refactor

--- src/App.js
+++ src/App.js
@@ -14,52 +14,51 @@ class App extends Component {
     this.state = {
       createFromTemplate: false,
       showSidebar: true,
-      elements: [],
-      deltaPosition: { x: 0, y: 0 }
+      elements: {},
+      menus: []
     };
 
     this.onPropChange = this.onPropChange.bind(this);
-
+    this.onDragStop = this.onDragStop.bind(this);
+    this.onResize = this.onResize.bind(this);
     this.createCard = this.createCard.bind(this);
+    this.loadLayout = this.loadLayout.bind(this);
   }
 
-  componentWillMount() {
-    if (this.state.createFromTemplate) {
-      const newCard0 = {
-        width: 2,
-        posx: 1,
-        posy: 2,
-        height: 13,
-        color: 0,
-        selected: false
-      };
-      const newCard = {
-        width: 10,
-        height: 1,
-        posx: 3,
-        posy: 2,
-        color: 1,
-        selected: false
-      };
-      const newCard2 = {
-        width: 10,
-        height: 10,
-        posx: 3,
-        posy: 3,
-        color: 2,
-        selected: false
-      };
+  loadLayout() {
+    const ref = JSON.parse(localStorage.getItem("elements"));
+    if (ref && Object.keys(ref).length > 0) {
+      const menus = Object.keys(ref).map(key => {
+        if (ref[key] !== null) {
+          ref[key].id = key;
+        }
+        return this.createMenu(key);
+      });
 
       this.setState({
-        elements: [newCard0, newCard, newCard2],
-        menus: [this.createMenu(0), this.createMenu(1), this.createMenu(2)]
+        elements: ref
       });
+
+      this.setState({ menus });
     }
   }
 
-  onPropChange(id, propName, val) {
-    let elements = [...this.state.elements];
-    elements[id][propName] = val;
+  onPropChange(id, updatedProps) {
+    // take copy of current state
+    const elements = { ...this.state.elements };
+    // update that state
+    elements[id] = { ...elements[id], ...updatedProps };
+
+    // set that to state
+    this.setState({ elements });
+  }
+
+  removeElement(key) {
+    // 1. take copy of state
+    const elements = { ...this.state.elements };
+    //2. update the state
+    elements[key] = null;
+
     this.setState({ elements });
   }
 
@@ -81,15 +80,27 @@ class App extends Component {
   }
 
   onCardClick(id) {
-    let elements = [...this.state.elements];
-    elements[id].selected = !elements[id].selected;
-    this.setState({ elements, showSidebar: true });
+    const elements = { ...this.state.elements };
+    // update that state
+    let updatedElement = elements[id];
+    if (updatedElement.hasGroup && updatedElement.hasGroup > 1) {
+      updatedElement.hasGroup.forEach(el => {
+        const element = { ...this.state.elements[el] };
+        // element.selected = !updatedElement.selected;
+        this.onPropChange(element.id, { selected: updatedElement.selected });
+      });
+    } else {
+      updatedElement.selected = !updatedElement.selected;
+    }
+    // set that to state
+    this.setState({ elements });
   }
 
   createMenu(x) {
-    if (this.state.elements.length > 0) {
+    if (this.state.elements[x]) {
       const options = [
         { value: "white", label: "white" },
+        { value: "black", label: "black" },
         { value: "grey", label: "grey" },
         { value: "red", label: "red" },
         { value: "orange", label: "orange" },
@@ -101,65 +112,127 @@ class App extends Component {
         { value: "pink", label: "pink" }
       ];
 
+      const element = this.state.elements[x];
+
       return (
         <div
           key={`menu-${x}`}
           className={`${
-            this.state.elements[x].selected ? "visible" : "hidden"
+            element.selected ? "visible" : "hidden"
           } border-b-4 p-4 border-grey`}
         >
-          <h2>Card {x + 1}</h2>
+          <h2>Card {x}</h2>
           <br />
-          <b>Width</b>
+          <b>border</b>
           <Slider
-            min={1}
-            max={12}
-            defaultValue={4}
-            onChange={e => this.onPropChange(x, "width", e)}
+            min={0}
+            max={1}
+            defaultValue={element.border}
+            onChange={e => this.onPropChange(x, { border: e })}
             handle={this.handle}
           />
           <br />
-          <b>Height</b>
+          <b>Shadow</b>
           <Slider
-            min={1}
-            max={13}
-            defaultValue={5}
-            onChange={e => this.onPropChange(x, "height", e)}
+            min={0}
+            max={2}
+            defaultValue={element.shadow}
+            onChange={e => this.onPropChange(x, { shadow: e })}
             handle={this.handle}
           />
           <br />
-          <b>Pos x</b>
+          <b>Rounded corners</b>
           <Slider
-            min={1}
-            max={13}
-            defaultValue={2}
-            onChange={e => this.onPropChange(x, "posx", e)}
-            handle={this.handle}
-          />
-          <br />
-          <b>Pos y</b>
-          <Slider
-            min={2}
-            max={13}
-            defaultValue={2}
-            onChange={e => this.onPropChange(x, "posy", e)}
+            min={0}
+            max={4}
+            defaultValue={element.radius}
+            onChange={e => this.onPropChange(x, { radius: e })}
             handle={this.handle}
           />
           <br />
           <button
-            onClick={() => {
-              console.log("remove");
-            }}
+            onClick={() => this.removeElement(x)}
             className="bg-red hover:bg-red-light text-white  py-2 px-4 border-b-4 border-red-dark hover:border-red rounded"
           >
             Remove card
           </button>
           <br />
+          <button
+            onClick={() => {
+              if (element.hasGroup && element.hasGroup.length > 1) {
+                const newElement = { ...element };
+                newElement.hasGroup = [];
+
+                element.hasGroup.forEach(async el => {
+                  const index = Object.keys(this.state.elements).length;
+
+                  const _element = this.state.elements[el];
+                  newElement.hasGroup.push(index);
+                  console.log(`pushing ${index} to hasGroup`);
+
+                  await this.createCard(index, {
+                    ..._element,
+                    x: _element.x + 20,
+                    y: _element.y + 20,
+                    id: index
+                  });
+                });
+                this.createCard(Object.keys(this.state.elements).length, {
+                  ...newElement,
+                  x: newElement.x + 20,
+                  y: newElement.y + 20,
+                  id: Object.keys(this.state.elements).length
+                });
+              } else {
+                this.createCard(Object.keys(this.state.elements).length, {
+                  ...element,
+                  x: element.x + 20,
+                  y: element.y + 20,
+                  id: Object.keys(this.state.elements).length
+                });
+              }
+            }}
+            className="bg-blue hover:bg-blue-light text-white  py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded"
+          >
+            Duplicate card
+          </button>
+
+          <br />
+          <button
+            onClick={() => {
+              if (!element.hasGroup && !element.groupedBy) {
+                const selectedElements = Object.keys(
+                  this.state.elements
+                ).reduce((prevArray, key) => {
+                  const el = { ...this.state.elements[key] };
+                  if (el.selected) {
+                    el.groupedBy = element.id;
+                    return [...prevArray, el.id];
+                  } else {
+                    return prevArray;
+                  }
+                }, []);
+                this.onPropChange(element.id, { hasGroup: selectedElements });
+              } else {
+                this.onPropChange(element.id, {
+                  groupedBy: null,
+                  hasGroup: null
+                });
+              }
+            }}
+            className="bg-blue hover:bg-blue-light text-white  py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded"
+          >
+            {element.hasGroup || element.groupedBy
+              ? "Ungroup cards"
+              : "Group cards"}
+          </button>
+          <br />
           <Select
-            value={this.state.selectedOption}
+            value={x.color}
             onChange={selectedOption =>
-              this.onPropChange(x, "color", selectedOption.value)
+              this.onPropChange(x, { color: selectedOption.value })
             }
+            className="text-black"
             options={options}
           />
         </div>
@@ -167,49 +240,77 @@ class App extends Component {
     }
   }
 
-  renderCard(cardType, x) {
+  onDragStop = (id, e, d) => {
+    const oldElement = { ...this.state.elements[id] };
+    this.onPropChange(id, { x: d.x, y: d.y });
+    if (oldElement.hasGroup && oldElement.hasGroup.length > 0) {
+      oldElement.hasGroup.forEach(el => {
+        const groupedElement = { ...this.state.elements[el] };
+        const delta = {
+          x: Math.abs(oldElement.x - groupedElement.x),
+          y: Math.abs(oldElement.y - groupedElement.y)
+        };
+        if (id !== el) {
+          this.onPropChange(el, {
+            x: d.x + delta.x,
+            y: d.y + delta.y
+          });
+        }
+      });
+    }
+  };
+
+  onResize = (id, e, direction, ref, delta, position) => {
+    this.onPropChange(id, {
+      width: ref.style.width,
+      height: ref.style.height,
+      ...position
+    });
+  };
+
+  renderCard(x) {
     return (
       <CardElement
-        key={`card-${x}`}
-        circle={cardType === "circle"}
-        width={this.state.elements[x].width}
-        height={this.state.elements[x].height}
+        key={x}
+        id={x}
         onClick={e => this.onCardClick(x, e)}
         selected={this.state.elements[x].selected}
         color={this.state.elements[x].color}
+        border={this.state.elements[x].border}
+        shadow={this.state.elements[x].shadow}
+        radius={this.state.elements[x].radius}
       />
     );
   }
 
-  createCard(cardType, x) {
-    const card = {
-      type: cardType,
-      width: 5,
-      height: 5,
-      posx: 120,
-      posy: 120,
-      color: "white",
-      selected: true
-    };
+  createCard(x, card = null) {
+    const element =
+      card !== null
+        ? card
+        : {
+            id: `${x}`,
+            x: 0,
+            y: 0,
+            width: 200,
+            height: 200,
+            color: "white",
+            selected: true
+          };
+
+    const elements = { ...this.state.elements };
+    elements[Object.keys(elements).length] = element;
 
     this.setState(prevState => {
-      return { elements: [...prevState.elements, card] };
+      return { elements, menus: [...prevState.menus, this.createMenu(x)] };
     });
   }
 
   render() {
-    let elements = [];
-    let menus = [];
-
-    for (let x = 0; x < this.state.elements.length; x++) {
-      menus.push(this.createMenu(x));
-      elements.push(this.renderCard(this.state.elements[x].type, x));
-    }
     return (
       <div className="App">
         {this.state.showSidebar && (
-          <div className="sidebar  p-5 bg-white">
-            <span
+          <div className="sidebar overflow-scroll p-5 text-white bg-grey-darkest">
+            {/* <span
               className="cursor-pointer block text-3xl text-right text-right"
               onClick={() =>
                 this.setState(prevState => {
@@ -218,31 +319,57 @@ class App extends Component {
               }
             >
               👈
-            </span>
-            <button
-              onClick={() => {
-                this.createCard("card", this.state.elements.length);
-              }}
-              className="bg-blue hover:bg-blue-light text-white font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded"
-            >
-              Create card
-            </button>
-            <button
-              onClick={() => {
-                this.createCard("circle", this.state.elements.length);
-              }}
-              className="bg-blue hover:bg-blue-light text-white font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded"
-            >
-              Create circle
-            </button>
+            </span> */}
+            <div>
+              <button
+                onClick={() => {
+                  this.createCard(Object.keys(this.state.elements).length);
+                }}
+                className="bg-blue hover:bg-blue-light text-white font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded"
+              >
+                Create card
+              </button>
+              <button
+                onClick={() => {
+                  localStorage.setItem(
+                    "elements",
+                    JSON.stringify(this.state.elements)
+                  );
+                }}
+                className="bg-blue hover:bg-blue-light text-white font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded"
+              >
+                Save layout
+              </button>
+              <button
+                onClick={this.loadLayout}
+                className="bg-blue hover:bg-blue-light text-white font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded"
+              >
+                Load layout
+              </button>
+              <button
+                onClick={() => {
+                  const elements = Object.keys(this.state.elements).map(key => {
+                    const el = { ...this.state.elements[key] };
+                    if (el !== null) {
+                      el.selected = false;
+                    }
+                    return el;
+                  });
+                  this.setState({ elements });
+                }}
+                className="bg-blue hover:bg-blue-light text-white font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded"
+              >
+                Deselect all
+              </button>
+            </div>
             <br />
-            {menus}
+            {this.state.menus.map((menu, i) => this.createMenu(i))}
           </div>
         )}
 
         <div
           style={{ gridColumn: this.state.showSidebar ? "2/-1" : "1/-1" }}
-          className="canvas p-b-2 flex bg-grey-darkest"
+          className="canvas p-b-2 flex bg-white"
         >
           <div className="menu-bar bg-grey-lighter flex px-4 py-3">
             <div className="mr-6">
@@ -252,7 +379,22 @@ class App extends Component {
             </div>
             <div className="flex-1 bg-white border border-grey-light rounded mr-4" />
           </div>
-          <div>{elements}</div>
+          <div>
+            {Object.keys(this.state.elements).map(
+              key =>
+                this.state.elements[key] && (
+                  <CardElement
+                    key={key}
+                    id={key}
+                    onClick={e => this.onCardClick(key, e)}
+                    selected={this.state.elements[key].selected}
+                    details={this.state.elements[key]}
+                    onDragStop={this.onDragStop}
+                    onResize={this.onResize}
+                  />
+                )
+            )}
+          </div>
         </div>
       </div>
     );
--- src/components/elements/card.js
+++ src/components/elements/card.js
@@ -6,37 +6,48 @@ class CardElement extends Component {
     super(props);
 
     this.state = { x: 0, y: 0, width: 200, height: 200 };
-    this.onResize = this.onResize.bind(this);
   }
 
-  onResize = (event, { element, size }) => {
-    this.setState({ width: size.width, height: size.height });
-  };
-
   render() {
+    const {
+      color,
+      height,
+      radius,
+      border,
+      shadow,
+      width,
+      x,
+      y
+    } = this.props.details;
+    const shadows = ["shadow", "shadow-md", "shadow-lg"];
+    const shadowIndex = shadows[shadow];
+
+    const radiusi = [
+      "rounded-none",
+      "rounded-sm",
+      "rounded",
+      "rounded-lg",
+      "rounded-full"
+    ];
+    const radiusIndex = radiusi[radius];
     return (
       <Rnd
-        size={{ width: this.state.width, height: this.state.height }}
-        position={{ x: this.state.x, y: this.state.y }}
-        onDragStop={(e, d) => {
-          this.setState({ x: d.x, y: d.y });
-        }}
-        onResize={(e, direction, ref, delta, position) => {
-          this.setState({
-            width: ref.style.width,
-            height: ref.style.height,
-            ...position
-          });
-        }}
+        size={{ width: width, height: height }}
+        position={{ x: x, y: y }}
+        onDragStop={(e, d) => this.props.onDragStop(this.props.id, e, d)}
+        onResize={(e, direction, ref, delta, position) =>
+          this.props.onResize(this.props.id, e, direction, ref, delta, position)
+        }
         bounds="parent"
-        resizeGrid={[25, 25]}
-        className={`${this.props.selected ? "border-2 border-black" : ""} bg-${
-          this.props.color
-        } cursor-pointer  ${this.props.circle && "rounded-full"}`}
+        resizeGrid={[20, 20]}
+        className={`${border === 1 &&
+          !this.props.selected &&
+          "border border-grey"} ${this.props.selected &&
+          "border-2 border-dashed border-red"} bg-${color} cursor-pointer ${radiusIndex} ${shadowIndex}`}
         onClick={this.props.onClick}
         style={{
-          width: this.state.width + "px",
-          height: this.state.height + "px"
+          width: this.props.width + "px",
+          height: this.props.height + "px"
         }}
       />
     );
@@ -53,11 +64,11 @@ CardElement.defaultProps = {
 
 CardElement.propTypes = {
   circle: PropTypes.bool,
-  height: PropTypes.number,
-  width: PropTypes.number,
   color: PropTypes.number,
   onClick: PropTypes.func,
-  selected: PropTypes.bool
+  selected: PropTypes.bool,
+  border: PropTypes.number,
+  shadow: PropTypes.number
 };
 
 export default CardElement;
